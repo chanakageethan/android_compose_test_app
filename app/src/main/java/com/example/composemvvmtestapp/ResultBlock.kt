26c8c66152e2e1e05ceef79e51e685ba917232e2
Geethan chanaka <geethan.chanaka@arimaclanka.com>
2024-09-05 20:16:09 +0530

    add convert part

---
+++ app/src/main/java/com/example/composemvvmtestapp/ResultBlock.kt
@@ -0,0 +1,44 @@
+package com.example.composemvvmtestapp
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun ResultBlock(message1: String, message2: String, modifier: Modifier=Modifier) {
+    Card(
+        elevation = CardDefaults.cardElevation(20.dp),
+        modifier = modifier.padding(0.dp, 20.dp, 0.dp, 0.dp)
+    ) {
+        Column(
+            modifier = modifier.padding(10.dp)
+        ) {
+            BasicText(
+                text = message1,
+
+
+                )
+
+            BasicText(
+                text = message2,
+                style = TextStyle(
+                    fontSize = 28.sp,
+                    color = Color.Blue,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+
+
+        }
+    }
+}
--- app/src/main/java/com/example/composemvvmtestapp/TopScreen.kt
+++ app/src/main/java/com/example/composemvvmtestapp/TopScreen.kt
@@ -1,24 +1,48 @@
 package com.example.composemvvmtestapp
+
+import android.icu.text.DecimalFormat
 import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import java.math.RoundingMode
 
 @Composable
 fun TopScreen(list: List<Conversion>) {
-    val selectedConversion :MutableState<Conversion?> =  remember{ mutableStateOf(null) }
-    val inputText:MutableState<String> = remember {
+    val selectedConversion: MutableState<Conversion?> = remember { mutableStateOf(null) }
+    val inputText: MutableState<String> = remember {
         mutableStateOf("")
     }
-    ConvertionMenu(list = list){
-                selectedConversion.value = it
+    val typedValue = remember { mutableStateOf("0.0") }
+
+    ConvertionMenu(list = list) {
+        selectedConversion.value = it
     }
     selectedConversion.value?.let {
-        InputBlock(conversion = it, inputText =inputText  ){input->
+        InputBlock(conversion = it, inputText = inputText) { input ->
 
-            Log.i("===",input)
+            typedValue.value = input
 
         }
+    }
+
+
+    if(typedValue.value != "0.0"){
+        val input = typedValue.value.toDouble()
+        val multiply = selectedConversion.value!!.multiplyBy
+        val result = input * multiply
+
+
+        val df = DecimalFormat("#.####")
+        df.roundingMode =RoundingMode.DOWN.ordinal
+        val roundedResult = df.format(result)
+
+        val message1 =  "${typedValue.value} ${selectedConversion.value!!.convertFrom} is equal to"
+        val message2 = "$roundedResult ${selectedConversion.value!!.convertTo}"
+
+        ResultBlock(message1 = message1, message2 = message2 )
+
+
     }
 }
